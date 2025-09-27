1597c8c20d41bd8836c43595b6067cc2925d9b80
FrancoCoria921 <francomartincoria921@gmail.com>
2025-09-27 00:23:49 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
+// myApp.js
 let express = require('express');
+
+// ------------------------------------
+console.log("Hello World"); 
+
 let app = express();
+// ... resto del archivo vacío ...
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
