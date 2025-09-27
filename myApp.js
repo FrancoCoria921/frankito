3e99b9f5931dd2b5673ac3d40743415d78912ea1
FrancoCoria921 <francomartincoria921@gmail.com>
2025-09-27 00:30:59 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,18 +6,23 @@ console.log("Hello World");
 let app = express();
 
 // ----------------------------------------------------------------------
-// MODIFICACIÓN DE RUTA: Sirve el archivo index.html
+// AÑADE ESTE CÓDIGO AQUÍ: Monta el middleware express.static
 // ----------------------------------------------------------------------
 
-// __dirname es la variable global de Node que contiene la ruta absoluta 
-// del directorio donde se encuentra myApp.js
-const absolutePath = __dirname + '/views/index.html'; 
+// 1. Calcula la ruta absoluta al directorio 'public'
+const absolutePathToPublic = __dirname + '/public';
+
+// 2. Monta el middleware usando app.use()
+// La ruta '/public' es el prefijo virtual que el cliente usará para acceder
+app.use('/public', express.static(absolutePathToPublic));
+
+// ----------------------------------------------------------------------
+
+// Definición de la ruta raíz (desafío anterior)
+const absolutePathToIndex = __dirname + '/views/index.html'; 
 
 app.get('/', function(req, res) {
-  // res.sendFile() requiere la ruta absoluta
-  res.sendFile(absolutePath);
+  res.sendFile(absolutePathToIndex);
 });
-
-// ----------------------------------------------------------------------
 
 module.exports = app;
