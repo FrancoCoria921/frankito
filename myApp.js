951b98ec83e33c93e8da6958c76c917984e74d9b
FrancoCoria921 <francomartincoria921@gmail.com>
2025-09-27 00:27:36 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,18 @@
 // myApp.js
 let express = require('express');
 
-// ------------------------------------
+// La línea console.log("Hello World"); del desafío anterior se queda aquí.
 console.log("Hello World"); 
 
 let app = express();
-// ... resto del archivo vacío ...
+
+// ----------------------------------------------------------------------
+// AÑADE ESTE CÓDIGO AQUÍ:
+// Define una ruta GET para la URL raíz (PATH: '/')
+app.get('/', function(req, res) {
+  // El método res.send() envía la cadena como respuesta HTTP
+  res.send('Hello Express');
+});
+// ----------------------------------------------------------------------
 
 module.exports = app;
