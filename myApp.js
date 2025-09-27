0a02b68c87a8efa5711b170cb42c9f37a294f7de
FrancoCoria921 <francomartincoria921@gmail.com>
2025-09-27 20:16:48 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,28 +5,37 @@ require('dotenv').config();
 
 let express = require('express');
 
-// Desafío "Bienvenido a la consola de Node"
 console.log("Hello World"); 
 
 let app = express();
 
-// 2. Sirve Recursos Estáticos (Middleware)
-// Monta el middleware express.static a la ruta /public
+// ----------------------------------------------------------------------
+// AÑADIDO: Middleware de Registro (Logger)
+// ----------------------------------------------------------------------
+app.use(function(req, res, next) {
+    // Formato requerido: method path - ip
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    
+    // Llamar a next() es crucial para pasar el control al siguiente handler (la ruta)
+    next();
+});
+// ----------------------------------------------------------------------
+
+
+// Configuración de recursos estáticos
 const absolutePathToPublic = __dirname + '/public';
 app.use('/public', express.static(absolutePathToPublic));
 
-// 3. Sirve un Archivo HTML (Ruta raíz /)
-// El __dirname + '/views/index.html'
+// Definición de la ruta raíz (desafío anterior)
 const absolutePathToIndex = __dirname + '/views/index.html'; 
 app.get('/', function(req, res) {
   res.sendFile(absolutePathToIndex);
 });
 
-// 4. Sirve JSON en una ruta específica (/json) y usa .env
+// Sirve JSON en una ruta específica (/json) y usa .env
 app.get('/json', function(req, res) {
   let message = "Hello json";
   
-  // Accede a process.env.MESSAGE_STYLE y aplica la lógica
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
